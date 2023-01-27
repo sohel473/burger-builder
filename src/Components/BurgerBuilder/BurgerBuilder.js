2fd902752d6ed2aa80b0cad0b85d00a6a2899f77
Sohel <absohel47@gmail.com>
2023-01-27 19:03:40 +0000

    Project Setup

---
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class BurgerBuilder extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Burger Builder</p>
+      </div>
+    );
+  }
+}
