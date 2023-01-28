0379c46d3c78d92863cbb761b4aa070a68bda962
Sohel <absohel47@gmail.com>
2023-01-28 20:39:23 +0000

    Create Ingredient Component

---
+++ src/Components/BurgerBuilder/Burger/Burger.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Ingredient from "../Ingredient/Ingredient";
+
+const Burger = (props) => {
+  return (
+    <div>
+      <Ingredient type="bread-top" />
+      <Ingredient type="cheese" />
+      <Ingredient type="salad" />
+      <Ingredient type="meat" />
+      <Ingredient type="bread-bottom" />
+      {/* <p>Burger</p> */}
+    </div>
+  );
+};
+
+export default Burger;
--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Burger from "./Burger/Burger";
 
 export default class BurgerBuilder extends React.Component {
   render() {
     return (
       <div>
-        <p>Burger Builder</p>
+        <Burger />
       </div>
     );
   }
